fa4c457083b123cfa39462867209b4be76d96301
SherTi <tulkunovsher69@gmail.com>
2023-03-08 17:19:50 +0500

    add mobile optimize

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 document.addEventListener('DOMContentLoaded' , () => {
 
     let backedIcon = document.querySelector('.backed-block-icon')
-    let backedNav = document.querySelector('.backed-block')
+    let backedNav = document.querySelector('.backed-block-container')
     backedIcon.addEventListener('click' , (event) =>{
+        console.log()
         backedNav.classList.add('active-nav')
     })
 })
