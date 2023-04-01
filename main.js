3c68f0b6f6374d8ad6316421e4854874b04c82df
SherTi <tulkunovsher69@gmail.com>
2023-04-01 18:43:48 +0500

    fixed bag

--- main.js
+++ main.js
@@ -8,5 +8,26 @@ document.addEventListener('DOMContentLoaded' , () => {
     })
         collapse.addEventListener('click' , () => {
             backedNav.classList.remove('active-nav')
-        })
+        });
+    const swiper = new Swiper('.swiper' ,{
+        direction: 'horizontal',
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+        loop: true,
+        effect: 'slide',
+        navigation: {
+            nextEl: '.btn-next',
+            prevEl: '.btn-prev',
+        },
+        pagination:{
+            clickable: true,
+            enabled: true,
+            type: "bullets",
+            bulletActiveClass: "slider-dots-active",
+            el: "#dots-container",
+            bulletClass: "s-dot"
+        }
+    });
+
+
 })
